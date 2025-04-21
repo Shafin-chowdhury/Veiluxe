90e7bf642ab17e992324aaf184e16919a2755154
Shafin chowdhury <ShafinChowdhury2001@gmail.com>
2025-04-21 17:27:38 +0600

    updated navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Armchair, Check, Info, Snail } from 'lucide-react'
+import { Armchair, Check, Info, Search, ShoppingBagIcon, ShoppingBasketIcon, Snail } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router'
 
@@ -27,15 +27,65 @@ const Navbar = () => {
 
       </div>
 
-      <div className="navbar_middle flex items-center justify-between">
-        <div className="lg:container">
-            <div className="logo_wrapper">
-                <Link to = "/" className='text-3xl text-black font-inter font-medium capitalize flex items-center gap-2'
-                <Snail />
-                Veiluxe 
 
+
+      {/* Navbar Main */}
+
+      <div className="navbar_middle flex items-center justify-center bg-slate-300 w-full h-[84px]">
+        <div className="lg:container grid grid-cols-3 items-center">
+            <div className="logo_wrapper">
+                <Link to = "/" className='text-3xl text-black font-inter font-medium capitalize flex items-center gap-2'>
+                <Snail />
+                Veiluxe
                 </Link>
             </div>
+
+
+
+            <div className='search_box'>
+              <form action="#" className='max-w-96 h-[44px] relative'>
+                <input type='text' placeholder='Search Here.....' className='w-full h-full bg-white rounded-lg pl-4' />
+                <button className='absolute to-50% right-4 translate-y-1/2'> <Search size='22px' /></button>
+              </form>
+            </div>
+
+
+            {/* <div className="navbar_middle_right flex items-center gap-6">
+              <button className='btn capitalize'>
+                <ShoppingBagIcon />Cart
+                <div className="badge badge-sm bg-slate-300">2</div>
+              </button>
+            </div> */}
+<div className="navbar_middle_right flex items-center gap-6">
+  <button className="btn capitalize flex items-center gap-2 relative">
+    <div className="relative rounded-full bg-gray-200 p-2 flex items-center justify-center">
+      <ShoppingBagIcon className="w-5 h-5 text-black" />
+      <div className="absolute -top-1 -right-1 bg-red-600 text-white rounded-full w-5 h-5 text-xs flex items-center justify-center">
+        2
+      </div>
+    </div>
+  </button>
+
+  <ul className="flex items-center gap-2">
+    <li className="rounded-full bg-gray-200 px-4 py-2 text-sm cursor-pointer hover:bg-gray-300">Sign In</li>
+    <li className="rounded-full bg-gray-200 px-4 py-2 text-sm cursor-pointer hover:bg-gray-300">Sign Out</li>
+  </ul>
+</div>
+   
+
+
+   {/* <div className='flex items-center gap-4' >
+    <ul className='flex items-center gap-2 relative ' >
+      <li className='relative rounded-full bg-gray-200 p-2 flex items-center justify-center'>Sign In</li>
+      <li className='relative rounded-full bg-gray-200 p-2 flex items-center justify-center'>Sign Out</li>
+    </ul>
+   </div> */}
+
+
+
+
+
+
         </div>
       </div>
     </div>
